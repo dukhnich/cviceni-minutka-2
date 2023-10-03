91b9c23711c9227a1d20d3d3be4134f47b31aa92
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-10-03 18:58:29 +0200

    add minutes functionality

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 'use strict';
-let times = 0;
+let secunds = 0;
+let minutes = 0;
+
 const alarm = document.querySelector('.alarm');
 const audio = document.querySelector('audio');
 const input = document.querySelector('.time-input');
 const secondsEl = document.querySelector('.alarm__seconds');
+const minutesEl = document.querySelector('.alarm__minutes');
 const form = document.querySelector('.controls');
 
-const changeSecunds = () => {
+const changeTime = () => {
     if (secondsEl) {
-        secondsEl.textContent = String(times).padStart(2, '0');
+        secondsEl.textContent = String(secunds).padStart(2, '0');
     }
+    if (minutesEl) {
+        minutesEl.textContent = String(minutes).padStart(2, '0');
+    }
+
 }
 if (input) {
     const onInput = () => {
-        times = Number(input.value);
-        changeSecunds();
+        if (Number(input.value) > 0) {
+            minutes = Number(input.value);
+            changeTime();
+        }
     };
     input.addEventListener('change', onInput)
 }
@@ -27,7 +36,7 @@ const startTimer = (e) => {
         alarm.classList.remove('alarm--ring');
     }
     const intervalId = setInterval(() => {
-        if (times === 0) {
+        if (minutes <= 0 && secunds <= 0) {
             clearInterval(intervalId);
             if (alarm) {
                 alarm.classList.add('alarm--ring');
@@ -37,8 +46,12 @@ const startTimer = (e) => {
             }
             return;
         }
-        times -= 1;
-        changeSecunds();
+        if (secunds === 0) {
+            minutes -= 1;
+            secunds = 60;
+        }
+        secunds -= 1;
+        changeTime();
     }, 1000)
 }
 if (form) {
