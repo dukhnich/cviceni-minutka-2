9deb513ae2a45a015fda9ec26be1de57c05108ab
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-10-03 18:14:32 +0200

    add base alarm functionality

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 'use strict';
+let times = 0;
+const alarm = document.querySelector('.alarm');
+const audio = document.querySelector('audio');
+const input = document.querySelector('.time-input');
+const secondsEl = document.querySelector('.alarm__seconds');
+const form = document.querySelector('.controls');
 
-console.log('funguju!');
+const changeSecunds = () => {
+    if (secondsEl) {
+        secondsEl.textContent = String(times).padStart(2, '0');
+    }
+}
+if (input) {
+    const onInput = () => {
+        times = Number(input.value);
+        changeSecunds();
+    };
+    input.addEventListener('change', onInput)
+}
+const startTimer = (e) => {
+    e.preventDefault();
+    if (input) {
+        input.value = '';
+    }
+    if (alarm) {
+        alarm.classList.remove('alarm--ring');
+    }
+    const intervalId = setInterval(() => {
+        if (times === 0) {
+            clearInterval(intervalId);
+            if (alarm) {
+                alarm.classList.add('alarm--ring');
+            }
+            if (audio) {
+                audio.play();
+            }
+            return;
+        }
+        times -= 1;
+        changeSecunds();
+    }, 1000)
+}
+if (form) {
+    form.addEventListener('submit', startTimer);
+}
